fc687b75969c6858da7f2df625ed84443eb4b7ef
Nekhala <72190612+Nekhala@users.noreply.github.com>
2021-05-07 00:04:13 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <p-toolbar/>
+    <v-main>
+      <!-- <HelloWorld/> -->
+      <router-view /> 
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import PToolbar from './components/PToolbar';
+// import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    // HelloWorld,
+    PToolbar,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style>
+
+
+
+</style>
